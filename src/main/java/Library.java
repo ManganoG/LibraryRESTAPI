402d2611ef6c3160b9570d3c416755c73d6b3acc
Mangano <ManganoGiulia@hotmail.com>
2022-03-29 08:54:21 +0000

    prestiti_p1

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -139,29 +139,36 @@ public class Library {
         return Response.ok(obj,MediaType.APPLICATION_JSON).build();
     }
 
+
+    private boolean checkParams2(int ID,int IDLibro, int IDUtente,String DataInizio,String DataFine){
+        return (ID == 0 || IDLibro == 0 || IDUtente == 0 || (DataInizio == null || DataInizio.trim().length() == 0) || (DataFine == null || DataFine.trim().length() == 0));
+    }
+
     @POST
     @Path("/newLead")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response addLead(@FormParam("IDLibro") String IDLibro,
-                           @FormParam("IDUtenti") String IDUtenti,
+    public Response addLead(@FormParam("ID") int ID,
+                            @FormParam("IDLibro") int IDLibro,
+                           @FormParam("IDUtenti") int IDUtenti,
                            @FormParam("DataInizio") String DataInizio,
                            @FormParam("DataFine") String DataFine){
-        if(checkParams(IDLibro, IDUtenti, DataInizio)) {
+        if(checkParams2(ID,IDLibro, IDUtenti, DataInizio,DataFine)) {
             String obj = new Gson().toJson("Parameters must be valid");
             return Response.serverError().entity(obj).build();
         }  
         /*Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(DataInizio); 
         Date DataF = date1.add(Calendar.MONTH, 1);*/
-        final String QUERY = "INSERT INTO Libri(IDLibro,IDUtenti,DataInizio,DataFine) VALUES(?,?,"+DataInizio+","+DataFine+")";
+        final String QUERY = "INSERT INTO Libri(ID,IDLibro,IDUtenti,DataInizio,DataFine) VALUES(?,?,?,?,?)";
         final String[] data = Database.getData();
         try(
 
                 Connection conn = DriverManager.getConnection(data[0]);
+
                 PreparedStatement pstmt = conn.prepareStatement( QUERY )
         ) {
-            pstmt.setString(1,IDLibro);
-            pstmt.setString(2,IDUtenti);
+            pstmt.setString(1,Integer.toString(IDLibro));
+            pstmt.setString(2,Integer.toString(IDUtenti));
             pstmt.setString(3,DataInizio);
             pstmt.setString(4,DataFine);
             pstmt.execute();
